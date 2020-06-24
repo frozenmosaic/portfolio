3df6ca573ef0659b5499224fcfe9d88e986eb837
Jeff Huynh <toibau2016@gmail.com>
2020-06-24 17:04:31 -0400

    removed extra

--- web_apps/dashboard/config.js
+++
@@ -1,4 +0,0 @@
-export default {
-    apiKey: 'AIzaSyDd5rRntqWv4Z2PqjMjiKsnc6ank2iszBw',
-    spreadsheetId: '1h0Dqzf1npSrDmBqxIZ7qpfRgm8SYf0Wci_tovkdTB7A'
-}
--- web_apps/dashboard/test.js
+++
@@ -1,6 +0,0 @@
-let value = Array(10);
-for (let i = 1; i < value.length; i++) {
-    for (let j=0; j < value++[++i].length; j++) {
-        console.log(i, j)
-    }
-}
